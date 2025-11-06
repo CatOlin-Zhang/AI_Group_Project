4e95ec99e33c34e6a07ccbffb1dbfb11638cd31d
阿根达斯 <2315916438@qq.com>
2025-11-06 20:20:01 +0800

    Initial commit

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ from scipy import stats
 import seaborn as sns
 import re
 import spacy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings('ignore')
