74b2f93ef1321bfcc0e077933504972296c931ef
luckylochi <50609924+luckylochi@users.noreply.github.com>
2025-11-18 16:13:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import torch
-# 超参数配置
-# 设备设置
+# Hyperparameter Configuration
+# device set
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
-# 路径与参数
+# Path and Parameters
 model_name = 'bert-base-uncased'
 max_length = 512
 batch_size = 16
